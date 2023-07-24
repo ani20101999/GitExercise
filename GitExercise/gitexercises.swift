294744f4f2e905c8b0bff86c876ca07078122b5e
Animesh <animesh.rout@travelxp.tv>
2023-07-24 12:01:53 +0530

    made changes

---
+++ GitExercise/gitexercises.swift
@@ -0,0 +1,20 @@
+//
+//  gitexercises.swift
+//  GitExercise
+//
+//  Created by Animesh Rout on 24/07/23.
+//
+
+import SwiftUI
+
+struct gitexercises: View {
+    var body: some View {
+        Text("Hello, World!lovely day,how r you")
+    }
+}
+
+struct gitexercises_Previews: PreviewProvider {
+    static var previews: some View {
+        gitexercises()
+    }
+}
