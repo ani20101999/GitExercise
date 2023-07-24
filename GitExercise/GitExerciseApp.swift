890a0cc3ea6b7133dbdb68c96a8909fc095307d4
Animesh <animesh.rout@travelxp.tv>
2023-07-24 11:47:07 +0530

    Initial Commit

---
+++ GitExercise/GitExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitExerciseApp.swift
+//  GitExercise
+//
+//  Created by Animesh Rout on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GitExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
